4d1b261541a49051fb4313b55b4248f20116294e
sudons <sudons@msn.cn>
2022-12-31 22:10:42 +0800

    服务定位器ServiceLocator

---
+++ ServiceLocatorDemo/Program.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+namespace ServiceLocatorDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceCollection services = new ServiceCollection();
+            services.AddTransient<TestServiceImp>();
+            using(ServiceProvider sp=services.BuildServiceProvider())
+            {
+                TestServiceImp testService = sp.GetRequiredService<TestServiceImp>();
+                testService.Name = "云文";
+                testService.SayHi();
+            }
+        }
+    }
+}
---
+++ ServiceLocatorDemo/TestServiceImp.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceLocatorDemo
+{
+    internal class TestServiceImp : ITestService,IDisposable
+    {
+        public string Name { get; set ; }
+        public void SayHi()
+        {
+            Console.WriteLine($"Hi,I'm {Name}");
+        }
+        /// <summary>
+        /// 被依赖注入窗口管理的类如果实现了IDisposable接口，则离开作用域之后容器会自动调用对象的Dispose方法,并及时释放非托管资源
+        /// </summary>
+        public void Dispose()
+        {
+            Console.WriteLine($"我实现了IDisposable接口");
+        }
+    }
+}
