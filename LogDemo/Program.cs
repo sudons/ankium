b0d80be30327d0a03c9f26c2763bf2fed8640a15
sudons <sudons@msn.cn>
2023-01-03 23:54:40 +0800

    简单的日志程序

---
+++ LogDemo/Program.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System.Runtime.CompilerServices;
+
+namespace LogDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceCollection services = new ServiceCollection();
+            services.AddLogging(logBuilder => { logBuilder.AddConsole(); });
+            using(ServiceProvider sp = services.BuildServiceProvider())
+            {
+                ILogger logger= sp.GetRequiredService<ILogger<Program>>();
+                logger.LogWarning("这是一条警告消息");
+                logger.LogError("这是一条错误消息");
+                string age = "abc";
+                logger.LogInformation($"用户输入的年龄：{age}");
+                try
+                {
+                    int i = int.Parse(age);
+                }
+                catch (Exception ex)
+                {
+
+                    logger.LogError(ex, "解析字符串为int失败");
+                }
+            }
+        }
+    }
+}
