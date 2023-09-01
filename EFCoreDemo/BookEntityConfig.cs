6545ad0501e0637a65143253e8f96a6fea2628ed
sudons <sudons@msn.cn>
2023-09-01 18:40:14 +0800

    添加新的EFCoreDemo项目，并根据Book实体类在MSSQLLocalDB实例的EFCoreDemo数据库中迁移生成数库表T_Books

---
+++ EFCoreDemo/BookEntityConfig.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace EFCoreDemo
+{
+    public class BookEntityConfig : IEntityTypeConfiguration<Book>
+    {
+        public void Configure(EntityTypeBuilder<Book> builder)
+        {
+            builder.ToTable("T_Books");
+        }
+    }
+}
---
+++ EFCoreDemo/HelloDbContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFCoreDemo
+{
+    internal class HelloDbContext:DbContext
+    {
+        //定义实体在数据库中的默认表名
+        public DbSet<Book> Books { get; set; }
+        //配置数据库连接字符串
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string conStr = @"Server=(localdb)\MSSQLLocalDB;DataBase=EFCoreDemo;Trusted_Connection=True;TrustServerCertificate=True";
+            optionsBuilder.UseSqlServer(conStr);
+        }
+        //加载当前程序集中所有实现了IEntityTypeConfiguration接口的类
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(this.GetType().Assembly);
+        }
+    }
+}
